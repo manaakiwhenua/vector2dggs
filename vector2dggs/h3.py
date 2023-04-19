d567df92aab80a2fc0eb8298954290411eade885
alpha-beta-soup <richard.m.law@gmail.com>
2023-04-20 09:03:09 +1200

    separate into separate cli tool for h3

---
+++ vector2dggs/h3.py
@@ -0,0 +1,208 @@
+import click
+import click_log
+import errno
+import logging
+import os
+import multiprocessing
+from multiprocessing.dummy import Pool
+from pathlib import Path
+import tempfile
+from typing import Union
+from urllib.parse import urlparse
+import warnings
+
+import dask.dataframe as dd
+import dask_geopandas as dgpd
+import geopandas as gpd
+import h3pandas
+import pandas as pd
+from shapely.geometry import GeometryCollection
+from tqdm import tqdm
+from tqdm.dask import TqdmCallback
+
+from . import katana
+
+LOGGER = logging.getLogger(__name__)
+click_log.basic_config(LOGGER)
+MIN_H3, MAX_H3 = 0, 15
+
+LOGGER.info('hello')
+
+def _index(
+    input_file: Union[Path, str],
+    output_directory: Union[Path, str],
+    resolution: int,
+    id_field: str,
+    all_attributes: bool,
+    npartitions: int,
+    cut_threshold: int,
+    processes: int,
+    overwrite: bool
+) -> Path:
+    '''
+    TODO write a docstring
+    '''
+
+    df = gpd.read_file(input_file)
+    if id_field:
+        df = df.set_index(id_field)
+
+    if not all_attributes:
+        # Remove all attributes except the geometry
+        df = df.loc[:, ["geometry"]]
+
+    # Preparing dataframe to be sliced
+    df = df.explode(index_parts=False)
+    df = df.to_crs(2193)
+
+    LOGGER.info("Watch out for ninjas! (Cutting polygons)")
+    with tqdm(total=df.shape[0]) as pbar:
+        for index, row in df.iterrows():
+            df.loc[index, "geometry"] = GeometryCollection(katana.katana(row.geometry, cut_threshold))
+            pbar.update(1)
+
+    LOGGER.info("Preparing for spatial partitioning")
+    df = df.to_crs(4326)
+    df = df.explode(index_parts=False) # Explode from GeometryCollection
+    df = df.explode(index_parts=False) # Explode multipolygons to polygons
+    df = df.reset_index()
+
+    ddf = dgpd.from_geopandas(df, npartitions=npartitions)
+
+    LOGGER.info("Spatial partitioning (Hilbert curve) with %d partitions", npartitions)
+    ddf = ddf.spatial_shuffle(by="hilbert", npartitions=npartitions)
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with TqdmCallback():
+            ddf.to_parquet(tmpdir)
+
+        filepaths = map(lambda f: f.absolute(), Path(tmpdir).glob('*'))
+
+        # TODO
+        # if overwrite:
+
+        # Polyfilling function defined here
+        def polyfill(pq_in):
+            """
+            Reads a geoparquet, performs H3 polyfilling,
+            and writes out to parquet.
+            """
+            df = gpd.read_parquet(pq_in)
+            # df = df[df.geometry.type == "Polygon"]
+            with warnings.catch_warnings():
+                # TODO does not seem to work
+                warnings.filterwarnings("ignore")
+                h3geo = df.h3.polyfill_resample(resolution, return_geometry=False)
+            h3geo = pd.DataFrame(h3geo).drop(columns=["hilbert_distance", "geometry"])
+            h3geo.to_parquet(
+                output_directory,
+                engine='auto',
+                compression='ZSTD'
+            )
+
+        # Multithreaded polyfilling
+        LOGGER.info(
+            "H3 Indexing on spatial partitions by polyfill with H3 resoltion: %d",
+            resolution,
+        )
+        # TODO progress bar
+        with Pool(processes=processes) as pool:
+            # have your pool map the file names to dataframes
+            # pool.map(polyfill, filepaths)
+            list(tqdm(pool.imap(polyfill, filepaths), total=npartitions))
+
+@click.command(context_settings={"show_default": True})
+@click_log.simple_verbosity_option(LOGGER)
+@click.argument("vector_input", required=True, type=click.Path(), nargs=1)
+@click.argument("output_directory", required=True, type=click.Path(), nargs=1)
+@click.option(
+    "-r",
+    "--resolution",
+    required=True,
+    type=click.Choice(list(map(str, range(MIN_H3, MAX_H3 + 1)))),
+    help="H3 resolution to index",
+    nargs=1,
+)
+@click.option(
+    "-id",
+    "--id_field",
+    required=False,
+    default=None,
+    type=str,
+    help="Field to use as an ID; defaults to a constructed single 0...n index on the original feature order.",
+    nargs=1,
+)
+@click.option(
+    "-a",
+    "--all_attributes",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Retain attributes in output. The default is to create an output that only includes H3 cell ID and the ID(s) given by the -id field (or the default index ID).",
+)
+@click.option(
+    "-p",
+    "--partitions",
+    required=True,
+    type=int,
+    default=50,
+    help="Geo-partitioning, currently only available in Hilbert method",
+    nargs=1,
+)
+@click.option(
+    "-c",
+    "--cut_threshold",
+    required=True,
+    default=5000,
+    type=int,
+    help="Cutting up large polygons into target length (meters)",
+    nargs=1,
+)
+@click.option(
+    "-t",
+    "--threads",
+    required=False,
+    default=7,
+    type=int,
+    help="Amount of threads used for operation",
+    nargs=1,
+)
+@click.option("-o", "--overwrite", is_flag=True)
+def h3(
+    vector_input: Union[str, Path],
+    output_directory: Union[str, Path],
+    resolution: str,
+    id_field : str,
+    all_attributes: bool,
+    partitions: int,
+    cut_threshold: int,
+    threads: int,
+    overwrite: bool
+):
+    """
+    Ingest a vector dataset and index it to the H3 DGGS.
+
+    VECTOR_INPUT is the path to input vector geospatial data.
+    OUTPUT_DIRECTORY should be a directorty, not a file, as it will be the write location for an Apache Parquet data store.
+    """
+    if not Path(vector_input).exists():
+        if not urlparse(vector_input).scheme:
+            LOGGER.warning(
+                f"Input vector {vector_input} does not exist, and is not recognised as a remote URI"
+            )
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), input_file)
+        vector_input = str(vector_input)
+    else:
+        vector_input = Path(vector_input)
+
+    _index(
+        vector_input,
+        output_directory,
+        int(resolution),
+        id_field,
+        all_attributes,
+        partitions,
+        cut_threshold,
+        threads,
+        overwrite
+    )
---
+++ vector2dggs/katana.py
@@ -0,0 +1,53 @@
+"""
+Copyright (c) 2016, Joshua Arnott
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from shapely.geometry import box, Polygon, MultiPolygon, GeometryCollection
+
+def katana(geometry, threshold, count=0) -> GeometryCollection:
+    """Split a Polygon into two parts across it's shortest dimension"""
+    bounds = geometry.bounds
+    width = bounds[2] - bounds[0]
+    height = bounds[3] - bounds[1]
+    if max(width, height) <= threshold or count == 250:
+        # either the polygon is smaller than the threshold, or the maximum
+        # number of recursions has been reached
+        return [geometry]
+    if height >= width:
+        # split left to right
+        a = box(bounds[0], bounds[1], bounds[2], bounds[1]+height/2)
+        b = box(bounds[0], bounds[1]+height/2, bounds[2], bounds[3])
+    else:
+        # split top to bottom
+        a = box(bounds[0], bounds[1], bounds[0]+width/2, bounds[3])
+        b = box(bounds[0]+width/2, bounds[1], bounds[2], bounds[3])
+    result = []
+    for d in (a, b,):
+        c = geometry.intersection(d)
+        if not isinstance(c, GeometryCollection):
+            c = [c]
+        for e in c:
+            if isinstance(e, (Polygon, MultiPolygon)):
+                result.extend(katana(e, threshold, count+1))
+    if count > 0:
+        return result
+    # convert multipart into singlepart
+    final_result = []
+    for g in result:
+        if not g.is_valid:
+            print('invalid')
+            print(g)
+        if isinstance(g, MultiPolygon):
+            final_result.extend(g)
+        else:
+            final_result.append(g)
+    return final_result
