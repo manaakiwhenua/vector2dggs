ec3112267dc237915b217f6172e82a97bb294b0d
alpha-beta-soup <richard.m.law@gmail.com>
2026-05-14 12:44:13 +1200

    black

--- vector2dggs/indexers/a5vectorindexer.py
+++ vector2dggs/indexers/a5vectorindexer.py
@@ -28,14 +28,25 @@ class A5VectorIndexer(VectorIndexer):
 
     @staticmethod
     def _polyfill_polygon(geom, resolution: int) -> list:
-        cells = set(a5.uncompact(a5.polygon_to_cells(list(geom.exterior.coords), resolution), resolution))
+        cells = set(
+            a5.uncompact(
+                a5.polygon_to_cells(list(geom.exterior.coords), resolution), resolution
+            )
+        )
         for interior in geom.interiors:
-            cells -= set(a5.uncompact(a5.polygon_to_cells(list(interior.coords), resolution), resolution))
+            cells -= set(
+                a5.uncompact(
+                    a5.polygon_to_cells(list(interior.coords), resolution), resolution
+                )
+            )
         return [a5.u64_to_hex(c) for c in cells]
 
     @staticmethod
     def _linetrace(geom, resolution: int) -> list:
-        return [a5.u64_to_hex(c) for c in a5.line_string_to_cells(list(geom.coords), resolution)]
+        return [
+            a5.u64_to_hex(c)
+            for c in a5.line_string_to_cells(list(geom.coords), resolution)
+        ]
 
     def polyfill(self, df: gpd.GeoDataFrame, resolution: int) -> pd.DataFrame:
         geom_col = df.geometry.name
@@ -62,7 +73,9 @@ class A5VectorIndexer(VectorIndexer):
                 self._geo_to_cells(
                     df_point,
                     resolution,
-                    lambda geom, res: [a5.u64_to_hex(a5.lonlat_to_cell((geom.x, geom.y), res))],
+                    lambda geom, res: [
+                        a5.u64_to_hex(a5.lonlat_to_cell((geom.x, geom.y), res))
+                    ],
                     geom_col,
                 )
             )
@@ -71,13 +84,17 @@ class A5VectorIndexer(VectorIndexer):
 
     def secondary_index(self, df: pd.DataFrame, parent_res: int) -> pd.DataFrame:
         df[f"a5_{parent_res:02}"] = df.index.map(
-            lambda cell: a5.u64_to_hex(a5.cell_to_parent(a5.hex_to_u64(cell), parent_res))
+            lambda cell: a5.u64_to_hex(
+                a5.cell_to_parent(a5.hex_to_u64(cell), parent_res)
+            )
         )
         return df
 
     def compaction(self, df, res, col_order, dggs_col, id_field):
         def _compact_hex(cells):
-            return [a5.u64_to_hex(c) for c in a5.compact([a5.hex_to_u64(c) for c in cells])]
+            return [
+                a5.u64_to_hex(c) for c in a5.compact([a5.hex_to_u64(c) for c in cells])
+            ]
 
         def _child_hex(cell, res):
             return a5.u64_to_hex(a5.cell_to_children(a5.hex_to_u64(cell), res)[0])
